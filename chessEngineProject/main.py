0ffb7bd5ff11b983b88a44440d99079fcf060b1b
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2023-07-11 21:56:21 +0800

    Change some variable names
    + display possibilities calculated in each turn

--- chessEngineProject/main.py
+++ chessEngineProject/main.py
@@ -1,10 +1,12 @@
-import chess, math, os
-board = chess.Board()
+import chess, math, os, copy
+chessboard = chess.Board()
 piece_worths = {"K": 0, "k": 0, "Q": 9, "q": -9, "B": 3, "b": -3, "N": 3, "n": -3,"R": 4, "r": -4, "P": 1, "p": -1}
 DEPTH = 2
-
+possibilities = 0
 
 def calc_advantage(board):
+    global possibilities
+    possibilities += 1
     piece_worth_value = 0
     for p in list(piece_worths.keys()):
         piece_worth_value += piece_worths.get(p)*str(board).count(p)
@@ -16,8 +18,9 @@ def calc_advantage(board):
     return piece_worth_value
 
 
-def best_value(board, is_white, depth):
     if depth == 0:
+def best_value(input_board, is_white, depth):
+    board = copy.copy(input_board)
         return calc_advantage(board)
     possible_moves = board.legal_moves
     best_move_values = []
@@ -52,18 +55,20 @@ def evaluate(board):
     return best_move
 
 
-best_value(board, True, DEPTH)
+best_value(chessboard, True, DEPTH)
 game_over_reasons = ("Checkmate", "Stalemate", "Insufficient material", "75-move rule", "Fivefold repetition", "50-move rule", "Threefold repetition")
 while True:
-    # input("Continue>>>")
+    print(f"Possibilities considered: {possibilities}")
+    possibilities = 0
     print("Calculating...")
-    board.push_san(str(evaluate(board)))
+    chessboard.push_san(str(evaluate(chessboard)))
     os.system("clear")
-    print(("Black to move", "White to move")[board.turn])
-    print(board)
-    if board.is_game_over():
-        print(game_over_reasons[board.outcome().termination.value-1])
-        print(board.outcome().result())
+    print(("Black to move", "White to move")[chessboard.turn])
+    print(chessboard)
+    if chessboard.is_game_over():
+        print(game_over_reasons[chessboard.outcome().termination.value-1])
+        print(chessboard.outcome().result())
         break
+
 
 
