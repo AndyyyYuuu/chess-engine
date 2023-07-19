ba4bc612aa197f1e119961050375f3188e028e36
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2023-07-20 07:32:28 +0800

    Add `play.py` and `main.py`

---
+++ chessEngineProject/main.py
@@ -0,0 +1,6 @@
+# Play Against StockYu Engine
+# Andy Yu
+# Play against StockYu using UCI notation
+
+import chess
+import stockyu
---
+++ chessEngineProject/play.py
@@ -0,0 +1,29 @@
+# Play Against StockYu Engine
+# Andy Yu
+# Play against StockYu using SAN notation
+
+import chess
+import stockyu
+import os
+
+board = chess.Board()
+
+while not board.is_game_over():
+    os.system("clear")
+    print("Your move!")
+    print(board)
+    while True:
+        user = input(">>> ")
+        try:
+            board.push_san(user)
+            break
+        except:
+            print("Error!")
+    os.system("clear")
+    print("Computer is thinking...")
+    print(board)
+    if board.is_game_over():
+        break
+    move = stockyu.evaluate(board)
+    board.push_san(str(move))
+print("Game over!")
