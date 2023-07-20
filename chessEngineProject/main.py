4ca04404ebc6f550471d75d04acfae14ea882ff5
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2023-07-20 13:03:11 +0800

    Write `main.py`

--- chessEngineProject/main.py
+++ chessEngineProject/main.py
@@ -1,6 +1,29 @@
-# Play Against StockYu Engine
+# StockYu Chess Engine
 # Andy Yu
-# Play against StockYu using UCI notation
+# StockYu in UCI Protocol
 
 import chess
 import stockyu
+board = None
+while True:
+    cmd = input().split()
+    try:
+        if cmd[0] == "uci":
+            print("id name StockYu")
+            print("id author Andy Yu")
+            print("uciok")
+        elif cmd[0] == "isready":
+            print("isreadyok")
+        elif cmd[0] == "ucinewgame":
+            board = chess.Board()
+        elif cmd[0] == "position":
+            if cmd[1] == "moves":
+                board.push_uci(cmd[2])
+            elif cmd[1] == "startpos":
+                board.reset_board()
+        elif cmd[0] == "go":
+            print(f"bestmove {stockyu.evaluate(board)}")
+        elif cmd[0] == "quit":
+            exit()
+    except:
+        pass
