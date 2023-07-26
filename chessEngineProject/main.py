b0136dacfafe2d4c7d06a0bb03c17b9a344837b7
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2023-07-26 11:02:29 +0800

    Update main.py

--- chessEngineProject/main.py
+++ chessEngineProject/main.py
@@ -11,32 +11,32 @@ board = None
 time_left = 1800
 while True:
     cmd = input().split()
-    try:
-        if cmd[0] == "uci":
-            print("id name StockYu")
-            print("id author Andy Yu")
-            print("uciok")
-        elif cmd[0] == "isready":
-            print("isreadyok")
-        elif cmd[0] == "ucinewgame":
-            board = chess.Board()
-        elif cmd[0] == "position":
-            if cmd[1] == "moves":
-                for i in cmd[2:]:
+    if cmd[0] == "uci":
+        print("id name StockYu")
+        print("id author Andy Yu")
+        print("uciok")
+    elif cmd[0] == "isready":
+        print("isreadyok")
+    elif cmd[0] == "ucinewgame":
+        board = chess.Board()
+    elif cmd[0] == "position":
+        if cmd[1] == "moves":
+            for i in cmd[2:]:
+                try:
                     board.push_uci(i)
-            elif cmd[1] == "startpos":
-                board.reset_board()
-        elif cmd[0] == "go":
-            # Make best move with time control
-            timestamp = time()
-            if time_left > 600:
-                print(f"bestmove {stockyu.evaluate(board, 3)}")
-            elif time_left > 90:
-                print(f"bestmove {stockyu.evaluate(board, 2)}")
-            else:
-                print(f"bestmove {stockyu.evaluate(board, 1)}")
-            time_left -= time() - timestamp
-        elif cmd[0] == "quit":
-            exit()
-    except:
-        pass
+                except:
+                    pass
+        elif cmd[1] == "startpos":
+            board.reset_board()
+    elif cmd[0] == "go":
+        # Make best move with time control
+        timestamp = time()
+        if time_left > 600:
+            print(f"bestmove {stockyu.evaluate(board, 3)}")
+        elif time_left > 90:
+            print(f"bestmove {stockyu.evaluate(board, 2)}")
+        else:
+            print(f"bestmove {stockyu.evaluate(board, 1)}")
+        time_left -= time() - timestamp
+    elif cmd[0] == "quit":
+        exit()
