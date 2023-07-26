68e583611fbe9cd78d558771a324266133ec144a
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2023-07-26 22:03:55 +0800

    Add support for FEN input + minor fixes
    Also fix error handling when making moves

--- chessEngineProject/main.py
+++ chessEngineProject/main.py
@@ -14,7 +14,7 @@ while True:
     cmd = input().split()
     if cmd[0] == "uci":
         print("id name StockYu")
-        print("id author Andy Yu")
+        print("id author Andy_Yu")
         print("uciok")
     elif cmd[0] == "isready":
         print("isreadyok")
@@ -29,6 +29,9 @@ while True:
                     pass
         elif cmd[1] == "startpos":
             board.reset_board()
+        else:
+            try: board.set_fen(cmd[1])
+            except:pass
     elif cmd[0] == "go":
         # Make best move with time control
         print(cmd)
@@ -39,22 +42,24 @@ while True:
                 except IndexError:
                     pass
         timestamp = time()
-        if time_update is None:
+        try:
+            if time_update is None:
 
-            if time_left > 600:
-                print(f"bestmove {stockyu.evaluate(board, 3)}")
-            elif time_left > 90:
-                print(f"bestmove {stockyu.evaluate(board, 2)}")
+                if time_left > 600:
+                    print(f"bestmove {stockyu.evaluate(board, 3)}")
+                elif time_left > 90:
+                    print(f"bestmove {stockyu.evaluate(board, 2)}")
+                else:
+                    print(f"bestmove {stockyu.evaluate(board, 1)}")
+                time_left -= time() - timestamp
             else:
-                print(f"bestmove {stockyu.evaluate(board, 1)}")
-            time_left -= time() - timestamp
-        else:
-            if time_update > 600:
-                print(f"bestmove {stockyu.evaluate(board, 3)}")
-            elif time_update > 90:
-                print(f"bestmove {stockyu.evaluate(board, 2)}")
-            else:
-                print(f"bestmove {stockyu.evaluate(board, 1)}")
-            time_update -= time() - timestamp
+                if time_update > 600:
+                    print(f"bestmove {stockyu.evaluate(board, 3)}")
+                elif time_update > 90:
+                    print(f"bestmove {stockyu.evaluate(board, 2)}")
+                else:
+                    print(f"bestmove {stockyu.evaluate(board, 1)}")
+                time_update -= time() - timestamp
+        except:pass
     elif cmd[0] == "quit":
         exit()
